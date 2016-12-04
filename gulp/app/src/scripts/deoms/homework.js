e9aa0a7b3275ad17026aaa327a97bdb2f1cb4fbb
269621270 <2696212170@qq.com>
2016-12-04 18:39:38 +0800

    config a fire 12-4

--- gulp/app/src/scripts/deoms/homework.js
+++ gulp/app/src/scripts/deoms/homework.js
@@ -6,25 +6,25 @@ angular.module("myApp",[])
             LastName:"vane",
             Gender:"Male",
             Salary1:"12,333.50",
-            Salary:"￥12,333.5",
-            Birthday:"2007-07-11"
+            Salary:"12,333.5",
+            Birthday:new Date("2007-07-11")
 
         },
         {
             FirstName:"SARA",
             LastName:"rose",
             Gender:"Female",
-            Salary1:"232,334.23",
-            Salary:"￥232,334.2",
-            Birthday:"1997-02-03"
+            Salary1:"23,334.23",
+            Salary:"23,334.2",
+            Birthday:new Date("1997-02-03")
         },
         {
             FirstName:"AAM",
             LastName:"hot",
             Gender:"Male",
             Salary1:"66,880.50",
-            Salary:"￥66,880.5",
-            Birthday:"1986-03-04"
+            Salary:"66,880.5",
+            Birthday:new Date("1986-03-04")
         }
         ,
         {
@@ -32,11 +32,11 @@ angular.module("myApp",[])
             LastName:"bear",
             Gender:"Male",
             Salary1:"68,000.00",
-            Salary:"￥68,000.0",
-            Birthday:"1968-03-22"
+            Salary:"68,000.0",
+            Birthday:new Date("1968-03-22")
         }
     ];
-    $scope.title=[
+  /*  $scope.title=[
         {
             id:0, title:"FirstName"
         },{
@@ -49,7 +49,7 @@ angular.module("myApp",[])
             id:4, title:"Salary"
         },{
             id:5, title:"Birthday"
-        }];
+        }];*/
 
     $scope.search = search;
     $scope.searchText="";
@@ -76,7 +76,7 @@ angular.module("myApp",[])
             return true;
         }
     }
-
+/*
     $scope.index=0;
     $scope.oder=false;
     //点击
@@ -100,7 +100,24 @@ angular.module("myApp",[])
     //显示隐藏
     $scope.cl=function(x){
         return this.addname===x;
-    };
+ };*/
+$scope.sortColum="FirstName"; //默认按照FirstName排序字段
+ $scope.reverseSort=false;//默认的排序方式为升序
+    //设置字段排序
+    $scope.setSort=function(column){  // column就是前面的值传过来字段
+        //如果传递过来的排序字段和当前字段一致，则要降序 否则升序
+        $scope.reverseSort=$scope.sortColum==column?!$scope.reverseSort:false;
+        //如果传递过来的排序字段和当前字段一致那么$scope.reverseSort就取他的相反的值，否则升序就是他原来默认的值
+        $scope.sortColum=column;  //点哪个让那个排序
+    }
+    //设置类
+    $scope.getClass=function(x){
+        if($scope.sortColum==x){
+            return $scope.reverseSort?"down":"up";
+            //判断$scope.reverseSort为真的话就降序否则就升序
+        }
+
+    }
 }])
 
 
